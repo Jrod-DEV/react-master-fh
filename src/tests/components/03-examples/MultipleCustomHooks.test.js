9c68b7c3130682e5b9e89bdb78dd706806181e27
Jonathan Rod <jonyrm91@gmail.com>
2021-12-08 18:57:11 +0100

    tests: testing MultipleCustomHooks, wrapper and info from useFetch

---
+++ src/tests/components/03-examples/MultipleCustomHooks.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { MultipleCustomHooks } from '../../../components/03-examples/MultipleCustomHooks';
+import { useFetch } from '../../../hooks/useFetch';
+jest.mock('../../../hooks/useFetch');
+
+describe('Pruebas en <MultipleCustomHooks/>', () => {
+  test('should show correctly', () => {
+    useFetch.mockReturnValue({
+      data: null,
+      loading: true,
+      error: null,
+    });
+
+    const wrapper = shallow(<MultipleCustomHooks />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  test('should show info', () => {
+    useFetch.mockReturnValue({
+      data: [
+        {
+          author: 'John',
+          quote: 'Hola Mundo',
+        },
+      ],
+      loading: false,
+      error: null,
+    });
+    const wrapper = shallow(<MultipleCustomHooks />);
+
+    expect(wrapper.find('.alert').exists()).toBe(false);
+    expect(wrapper.find('.mb-0').text().trim()).toBe('Hola Mundo');
+
+
+    console.log(wrapper.html());
+  });
+});
