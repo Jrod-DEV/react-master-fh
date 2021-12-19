f1558e34b60c16ea246c039baf25cd71eca86c78
Jonathan Rod <jonyrm91@gmail.com>
2021-12-19 20:43:46 +0100

    tests: test on todoReducer (delete and toggle)

--- src/tests/components/08-useReducer/todoReducer.test.js
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -21,4 +21,25 @@ describe('Pruebas en <todoReducer />', () => {
     const state = todoReducer(demoTodos, action);
     expect(state).toEqual([...demoTodos, newTodo]);
   });
+  test('should delete one TODO', () => {
+    //action.payload = ID del TODO
+    const action = {
+      type: 'delete',
+      payload: 1,
+    };
+    const state = todoReducer(demoTodos, action);
+    expect(state.length).toBe(1);
+    expect(state).toEqual([demoTodos[1]]);
+  });
+
+  test('should to the TOGGLE OF TODO', () => {
+    // action.payload = ID del TODO
+    const action = {
+      type: 'toggle',
+      payload: 1,
+    };
+    const state = todoReducer(demoTodos, action);
+    expect(state[0].done).toBe(true);
+    expect(state[1]).toEqual(demoTodos[1]);
+  });
 });
